328ea24b2571f2803e707d2f2a4a3f0fe60b8e29
Jack-tech707 <59619700+Jack-tech707@users.noreply.github.com>
2020-07-05 14:37:17 +0300

    Add files via upload

---
+++ areaTriangle.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    float base, height, area;
+    cout<<"Enter the base b=";
+    cin>>base;
+    cout<<"Enter the height h=";
+    cin>>height;
+    area=0.5*base*height;
+    cout<<"The area of the triangle is "<<area<<endl;
+    return 0;
+
+}
